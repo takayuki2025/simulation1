0ddb20c2eb8af7ca75a7d60bfb34e8fd530c6419
kawada takayuki <t.principle.k2024@gmail.com>
2025-09-18 01:47:29 +0900

    制作後調整06

--- src/resources/views/item_sell.blade.php
+++ src/resources/views/item_sell.blade.php
@@ -69,51 +69,48 @@
                                     <!-- <label>カテゴリーを選択してください:</label> -->
 <div class="category-buttons-container">
 
-<!-- <input type="hidden" name="category[]" value=""> -->
     <!-- ユニークなIDとラベル -->
-    <input type="checkbox" id="cat1" name="category[]" value="ファッション" class="category-checkbox-input">
+    <input type="checkbox" id="cat1" name="category[]" value="ファッション" class="category-checkbox-input" @if(is_array(old('category')) && in_array('ファッション', old('category'))) checked @endif>
     <label for="cat1" class="category-checkbox-label">ファッション</label>
     
-    <input type="checkbox" id="cat2" name="category[]" value="家電" class="category-checkbox-input">
+    <input type="checkbox" id="cat2" name="category[]" value="家電" class="category-checkbox-input" @if(is_array(old('category')) && in_array('家電', old('category'))) checked @endif>
     <label for="cat2" class="category-checkbox-label">家電</label>
     
-    <input type="checkbox" id="cat3" name="category[]" value="インテリア" class="category-checkbox-input">
+    <input type="checkbox" id="cat3" name="category[]" value="インテリア" class="category-checkbox-input" @if(is_array(old('category')) && in_array('インテリア', old('category'))) checked @endif>
     <label for="cat3" class="category-checkbox-label">インテリア</label>
 
-    <input type="checkbox" id="cat4" name="category[]" value="レディース" class="category-checkbox-input">
+    <input type="checkbox" id="cat4" name="category[]" value="レディース" class="category-checkbox-input" @if(is_array(old('category')) && in_array('レディース', old('category'))) checked @endif>
     <label for="cat4" class="category-checkbox-label">レディース</label>
     
-    <input type="checkbox" id="cat5" name="category[]" value="メンズ" class="category-checkbox-input">
+    <input type="checkbox" id="cat5" name="category[]" value="メンズ" class="category-checkbox-input" @if(is_array(old('category')) && in_array('メンズ', old('category'))) checked @endif>
     <label for="cat5" class="category-checkbox-label">メンズ</label>
     
-    <input type="checkbox" id="cat6" name="category[]" value="コスメ" class="category-checkbox-input">
+    <input type="checkbox" id="cat6" name="category[]" value="コスメ" class="category-checkbox-input" @if(is_array(old('category')) && in_array('コスメ', old('category'))) checked @endif>
     <label for="cat6" class="category-checkbox-label">コスメ</label>
 
-    <input type="checkbox" id="cat7" name="category[]" value="本" class="category-checkbox-input">
+    <input type="checkbox" id="cat7" name="category[]" value="本" class="category-checkbox-input" @if(is_array(old('category')) && in_array('本', old('category'))) checked @endif>
     <label for="cat7" class="category-checkbox-label">本</label>
     
-    <input type="checkbox" id="cat8" name="category[]" value="ゲーム" class="category-checkbox-input">
+    <input type="checkbox" id="cat8" name="category[]" value="ゲーム" class="category-checkbox-input" @if(is_array(old('category')) && in_array('ゲーム', old('category'))) checked @endif>
     <label for="cat8" class="category-checkbox-label">ゲーム</label>
     
-    <input type="checkbox" id="cat9" name="category[]" value="スポーツ" class="category-checkbox-input">
+    <input type="checkbox" id="cat9" name="category[]" value="スポーツ" class="category-checkbox-input" @if(is_array(old('category')) && in_array('スポーツ', old('category'))) checked @endif>
     <label for="cat9" class="category-checkbox-label">スポーツ</label>
 
-    <input type="checkbox" id="cat10" name="category[]" value="キッチン" class="category-checkbox-input">
+    <input type="checkbox" id="cat10" name="category[]" value="キッチン" class="category-checkbox-input" @if(is_array(old('category')) && in_array('キッチン', old('category'))) checked @endif>
     <label for="cat10" class="category-checkbox-label">キッチン</label>
 
-    <input type="checkbox" id="cat11" name="category[]" value="ハンドメイド" class="category-checkbox-input">
+    <input type="checkbox" id="cat11" name="category[]" value="ハンドメイド" class="category-checkbox-input" @if(is_array(old('category')) && in_array('ハンドメイド', old('category'))) checked @endif>
     <label for="cat11" class="category-checkbox-label">ハンドメイド</label>
 
-    <input type="checkbox" id="cat12" name="category[]" value="アクセサリー" class="category-checkbox-input">
+    <input type="checkbox" id="cat12" name="category[]" value="アクセサリー" class="category-checkbox-input" @if(is_array(old('category')) && in_array('アクセサリー', old('category'))) checked @endif>
     <label for="cat12" class="category-checkbox-label">アクセサリー</label>
 
-    <input type="checkbox" id="cat13" name="category[]" value="おもちゃ" class="category-checkbox-input">
+    <input type="checkbox" id="cat13" name="category[]" value="おもちゃ" class="category-checkbox-input" @if(is_array(old('category')) && in_array('おもちゃ', old('category'))) checked @endif>
     <label for="cat13" class="category-checkbox-label">おもちゃ</label>
 
-    <input type="checkbox" id="cat14" name="category[]" value="キッズ:ベビー" class="category-checkbox-input">
+    <input type="checkbox" id="cat14" name="category[]" value="キッズ:ベビー" class="category-checkbox-input" @if(is_array(old('category')) && in_array('キッズ:ベビー', old('category'))) checked @endif>
     <label for="cat14" class="category-checkbox-label">キッズ:ベビー</label>
-
-                     
 </div>
    @error('category')
                             <div class="error">{{ $message }}</div>
@@ -125,11 +122,11 @@
                     <div class="sell_title1_2">
                         <label>商品の状態</label>
                         <select name="condition" class="select_box">
-                            <option value="">選択してください</option>
-                            <option value="良好">良好</option>
-                            <option value="目立った傷や汚れなし">目立った傷や汚れなし</option>
-                            <option value="やや傷や汚れあり">やや傷や汚れあり</option>
-                            <option value="状態が悪い">状態が悪い</option>
+                            <option value="" @if(empty(old('condition'))) selected @endif>選択してください</option>
+                            <option value="良好" @if(old('condition') === '良好') selected @endif>良好</option>
+                            <option value="目立った傷や汚れなし" @if(old('condition') === '目立った傷や汚れなし') selected @endif>目立った傷や汚れなし</option>
+                            <option value="やや傷や汚れあり" @if(old('condition') === 'やや傷や汚れあり') selected @endif>やや傷や汚れあり</option>
+                            <option value="状態が悪い" @if(old('condition') === '状態が悪い') selected @endif>状態が悪い</option>
                         </select>
                                 @error('condition')
                                     <div class="error">{{ $message }}</div>
@@ -142,7 +139,7 @@
 
                     <div class="sell_title2_1">
                         <label>商品名</label>
-                        <input type="text" name="name" class="sell_item_form">
+                        <input type="text" name="name" class="sell_item_form" value="{{ old('name') }}">
                                 @error('name')
                                     <div class="error">{{ $message }}</div>
                                 @enderror
@@ -150,12 +147,12 @@
 
                     <div class="sell_title2_2">
                         <label>ブランド名</label>
-                        <input type="text" name="brand" class="sell_item_form">
+                        <input type="text" name="brand" class="sell_item_form" value="{{ old('brand') }}">
                     </div>
 
                     <div class="sell_title2_3">
                         <label>商品の説明</label>
-                        <textarea name="explain" class="sell_item_form_textarea"></textarea>
+                        <textarea name="explain" class="sell_item_form_textarea">{{ old('explain') }}</textarea>
                                 @error('explain')
                                     <div class="error">{{ $message }}</div>
                                 @enderror
@@ -163,10 +160,11 @@
 
                     <div class="sell_title2_4">
                         <label>販売価格</label>
-                        <div class="test">
-                        <span class="currency-symbol">¥</span>
-                        <input type="text" name="price" class="sell_item_form" value="">
-                </div>
+                        <!-- <div class="test"> -->
+                        
+                        <input type="text" name="price" class="sell_item_form2" value="{{ old('price') }}">
+<span class="currency-symbol">¥</span>
+                <!-- </div> -->
                                 @error('price')
                                     <div class="error">{{ $message }}</div>
                                 @enderror
